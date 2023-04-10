df405c0c19fce8c74e24657478e129d371c63b6c
Hiroki Shirokura <hiroki.shirokura@linecorp.com>
2023-04-10 09:43:41 +0900

    2023.04.10-09:43

--- python/scapy/main.py
+++ python/scapy/main.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
 from scapy.all import *
 
-src_mac = "aa:11:22:33:44:55"
-dst_mac = "aa:bb:cc:dd:ee:ff"
-src_ip = "192.168.1.1"
-dst_ip = "192.168.1.2"
-dst_port = 5678
-payload = "Hello, World!"
-src_srh = "::1"
-dst_srh = "2001:db8::3"
-sidlist = ['::1']
+dst_mac = "50:6b:4b:93:2a:3c"
+src_mac = "52:54:00:ab:ec:e7"
+src_ip = "10.0.0.101"
+dst_ip = "142.0.0.1"
+dst_port = 8080
+payload = "testdata"
+src_srh = "fc00:1100::"
+dst_srh = "fc00:aa01::"
+sidlist = ['fc00:aa01::']
 pcap_file = "output.pcap"
 
 packets = []
-for src_port in range(1024, 2048):
+for src_port in range(1024, 1024 + 500):
     tmp = Ether(src=src_mac, dst=dst_mac)
-    tmp = tmp / IPv6(src=src_ip, dst=dst_srh, nh=43, fl=src_port)
+    tmp = tmp / IPv6(src=src_srh, dst=dst_srh, nh=43, fl=src_port)
     tmp = tmp / IPv6ExtHdrSegmentRouting(segleft=len(sidlist)-1, lastentry=len(sidlist)-1, addresses=sidlist)
     tmp = tmp / IP(src=src_ip, dst=dst_ip)
     tmp = tmp / UDP(sport=src_port, dport=dst_port)
